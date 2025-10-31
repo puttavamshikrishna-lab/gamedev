df014f95acd5e90741fd1b8fe8cea5f41e8e784c
puttavamshikrishna-lab <putta.vamshikrishna@gmail.com>
2025-10-31 17:17:24 -0400

    Add files via upload

---
+++ nesteddic.py
@@ -0,0 +1,13 @@
+classroom = {
+"Jacob": {
+    "age": 16,
+    "marks": [35,90,89]
+},
+"Daniel":{
+    "age": 17,
+    "marks": [43,90,100]
+}
+}
+
+print(classroom["Jacob"]["age"])
+print(classroom["Daniel"]["marks"])
