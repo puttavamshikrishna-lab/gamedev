10db80aef7c8b76b7deb89faefb36a7cc8337f67
puttavamshikrishna-lab <putta.vamshikrishna@gmail.com>
2026-01-02 18:03:29 -0500

    Add files via upload

--- drawshapes.py
+++ drawshapes.py
@@ -1,26 +1,32 @@
 import pgzrun
-from random import randint 
+from random import randint
 from pgzero.builtins import Rect
-
-
-WIDTH=300
-HEIGHT=300
-
+WIDTH = 300
+HEIGHT = 300
 def draw():
-    screen.fill((0,0,0))
-
-    r=255
-    g=0
-    b=randint(120,255)
-
-
-    width=WIDTH
-    height=HEIGHT-200
-
-    corner1=(0,0)
-
+    screen.fill((0, 0, 0))     # black background
+    # initial color values
+    r = 255
+    g = 0
+    b = randint(120, 255)
+    # starting rectangle size
+    width = WIDTH
+    height = HEIGHT - 200
+    # starting corner
+    corner1 = (0, 0)
     for i in range(15):
-        corner2=(width,height)
-
-        rect=Rect(corner1,corner2)
-        rect.center=(150,150)
+        # ending corner
+        corner2 = (width, height)
+        # create rectangle object
+        rect = Rect(corner1, corner2)
+        # move rectangle to the center of the screen
+        rect.center = (150, 150)
+        # draw rectangle outline
+        screen.draw.rect(rect, (r, g, b))
+        # update width and height for next rectangle
+        width -= 10
+        height += 10
+        # adjust colors
+        r -= 10
+        g += 10
+pgzrun.go()
