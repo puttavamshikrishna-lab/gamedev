fcfe45f8641faa29ba4a0c8868a625db7795b6e4
puttavamshikrishna-lab <putta.vamshikrishna@gmail.com>
2026-04-24 18:02:00 -0400

    Add files via upload

--- connectingstars.py
+++ connectingstars.py
@@ -17,15 +17,15 @@ number_of_stars=7
 def create_stars():
     global start_time, stars, lines, next_star
 
-stars= []
-lines= []
-next_star=0
+    stars= []
+    lines= []
+    next_star=0
 
-for i in range(number_of_stars):
-    star=Actor("star")
-    star.pos = randint(60, WIDTH-60), randint(60, HEIGHT-60)
-    stars.append(star)
-start_time=time()
+    for i in range(number_of_stars):
+     star=Actor("star")
+     star.pos = randint(60, WIDTH-60), randint(60, HEIGHT-60)
+     stars.append(star)
+    start_time=time()
 
 def draw():
     global total_time
@@ -44,12 +44,52 @@ def draw():
 
         number +=1
 
-        for line in lines:
+    for line in lines:
             screen.draw.line(line[0], line[1], (255,255,255))
 
 
-        if next_star < number_of_stars:
+    if next_star < number_of_stars:
             total_time = time() - start_time 
+
+    screen.draw.text(
+               "Time: "+str(round(total_time,1)),
+               (10,10),
+               fontsize= 40,
+               color= "cyan",
+               owidth = 1.5,
+               ocolor = "Black"
+            )
+
+    if next_star < number_of_stars:
+                screen.draw.text(
+                    "Constellation completed",
+                    center = (WIDTH/2,50),
+                    fontsize= 50,
+                    color= "yellow",
+                    owidth= 1.5,
+                    ocolor="Black",
+
+                )
+
+def on_mouse_down(pos):
+                    global next_star
+                    if next_star < number_of_stars:
+                        if stars[next_star].collidepoint(pos):
+                            if next_star > 0:
+                                lines.append((stars[next_star-1],pos,stars[next_star.pos]))
+                            next_star += 1
+
+
+                    else:
+                            create_stars()
+
+
+create_stars()
+pgzrun.go()
+           
+
+
+
 
 
     
